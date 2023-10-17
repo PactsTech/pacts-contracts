a325902dca54ca53092eecd25fa73616b5f40eb1
Jake Breindel <j.breindel2@outlook.com>
2023-10-16 21:11:10 -0400

    feat: complete order processor

--- contracts/OrderProcessor.sol
+++ contracts/OrderProcessor.sol
@@ -17,24 +17,25 @@ enum State {
 }
 
 struct Order {
-    address immutable buyer;
-    address immutable shipper;
+    uint256 sequence;
+    address buyer;
+    address shipper;
+    uint256 price;
+    uint256 shipping;
     mapping(address => uint256) deposits;
-    uint256 createdBlock;
+    uint256 submittedBlock;
+    uint256 confirmedBlock;
     uint256 shippedBlock;
     uint256 deliveredBlock;
-    bool sellerShipped;
-    bool shipperReceived;
     State state;
     bytes shipment;
-    uint256 sequence;
     bytes metadata;
 }
 
 contract OrderProcessor {
     uint8 public constant VERSION = 1;
     uint256 public constant WAIT_BLOCKS = 21300;
-    
+
     uint256 sequence;
     address immutable seller;
     mapping(string => Order) orders;
@@ -75,15 +76,17 @@ contract OrderProcessor {
         address indexed _shipper,
         bytes _shipment
     );
-    event Aborted(
-        address indexed _buyer,
-        address indexed _seller,
-        string indexed _orderId
-    );
     event Canceled(
         address indexed _buyer,
         address indexed _seller,
-        string indexed _orderId
+        address indexed _shipper,
+        string _orderId
+    );
+    event Aborted(
+        address indexed _buyer,
+        address indexed _seller,
+        address indexed _shipper,
+        string _orderId
     );
     // event Disputed(
     //     address indexed _buyer,
@@ -97,116 +100,136 @@ contract OrderProcessor {
     error OnlyShipper();
     error InvalidState();
 
-    modifier onlyAfterCreated() {
-        require(
-            block.number >= (createdBlockNumber + WAIT_BLOCKS),
-            "Not long enough after created block"
-        );
-        _;
-    }
-
-    modifier onlyAfterShipped() {
-        require(
-            block.number >= (shippedBlockNumber + WAIT_BLOCKS),
-            "Not long enough after shipped block"
-        );
-        _;
-    }
-
-    modifier onlyAfterDelivered() {
-        require(
-            block.number >= (deliveredBlockNumber + WAIT_BLOCKS),
-            "Not long enough after delivered block"
-        );
-        _;
-    }
-
     constructor() payable {
+        sequence = 1;
         seller = msg.sender;
     }
 
-    function submit(string _orderId, address _shipper, bytes _metadata) external payable {
-        seq = sequence++;
-        orders[_orderId] = Order({
-            buyer: msg.sender,
-            shipper: _shipper,
-            createdBlock: block.number,
-            state: State.Submitted,
-            sequence: seq,
-            metadata: _metadata
-        });
+    function submit(
+        string memory _orderId,
+        address _shipper,
+        uint256 _price,
+        uint256 _shipping,
+        bytes memory _metadata
+    ) external payable {
+        require(
+            msg.value >= (_price + _shipping),
+            "Value is less than price + shipping"
+        );
+        orders[_orderId].sequence = sequence++;
+        orders[_orderId].buyer = msg.sender;
+        orders[_orderId].shipper = _shipper;
+        orders[_orderId].price = _price;
+        orders[_orderId].shipping = _shipping;
         orders[_orderId].deposits[msg.sender] = msg.value;
+        orders[_orderId].submittedBlock = block.number;
+        orders[_orderId].state = State.Submitted;
+        orders[_orderId].metadata = _metadata;
         emit Submitted(msg.sender, seller, _shipper, _orderId);
     }
 
-    function confirm() external payable onlySeller inState(State.Paid) {
-        require(msg.value >= price);
-        bytes memory array = bytes(order);
-        bytes memory encoded = abi.encodePacked(array);
-        emit Confirmed(buyer, seller, shipper, encoded);
-        state = State.Confirmed;
+    function confirm(string memory _orderId) external payable {
+        require(msg.sender == seller, "Only seller can confirm an order");
+        Order storage order = orders[_orderId];
+        require(msg.value >= order.price, "Not enough collateral");
+        require(order.sequence > 0, "Order does not exist");
+        require(order.state == State.Submitted, "Order in incorrect state");
+        orders[_orderId].deposits[msg.sender] = msg.value;
+        orders[_orderId].confirmedBlock = block.number;
+        orders[_orderId].state = State.Confirmed;
+        address buyer = order.buyer;
+        address shipper = order.shipper;
+        emit Confirmed(buyer, seller, shipper, _orderId);
     }
 
     function handOff(
+        string memory _orderId,
         bytes memory _shipment
-    ) external payable onlySeller inState(State.Confirmed) {
-        shipment = _shipment;
-        bytes memory array = bytes(shipment);
-        bytes memory encoded = abi.encodePacked(array);
-        emit HandedOff(buyer, seller, shipper, encoded);
-        state = State.HandedOff;
+    ) external payable {
+        require(msg.sender == seller, "Only seller can confirm an order");
+        Order storage order = orders[_orderId];
+        require(order.sequence > 0, "Order does not exist");
+        require(order.state == State.Confirmed, "Order in incorrect state");
+        orders[_orderId].shipment = _shipment;
+        orders[_orderId].state = State.HandedOff;
+        emit HandedOff(order.buyer, seller, order.shipper, _shipment);
     }
 
-    function ship() external payable onlyShipper inState(State.HandedOff) {
-        require(msg.value >= price);
-        emit Shipped(buyer, seller, shipper, shipment);
-        state = State.Shipped;
+    function ship(string memory _orderId) external payable {
+        Order storage order = orders[_orderId];
+        require(order.sequence > 0, "Order does not exist");
+        require(order.state == State.HandedOff, "Order in incorrect state");
+        require(order.shipper == msg.sender);
+        orders[_orderId].deposits[msg.sender] = msg.value;
+        orders[_orderId].shippedBlock = block.number;
+        emit Shipped(order.buyer, seller, order.shipper, order.shipment);
     }
 
-    function accept() external payable onlyBuyer inState(State.Shipped) {
-        emit Accepted(buyer, seller, shipper, shipment);
-        state = State.Accepted;
+    function accept(string memory _orderId) external payable {
+        Order storage order = orders[_orderId];
+        require(order.sequence > 0, "Order does not exist");
+        require(order.state == State.Shipped, "Order in incorrect state");
+        require(order.buyer == msg.sender);
+        orders[_orderId].state = State.Accepted;
+        emit Accepted(order.buyer, seller, order.shipper, order.shipment);
     }
 
-    function deliver() external payable onlyShipper inState(State.Shipped) {
-        emit Delivered(buyer, seller, shipper, shipment);
-        deliveredBlockNumber = block.number;
-        state = State.Delivered;
+    function deliver(string memory _orderId) external payable {
+        Order storage order = orders[_orderId];
+        require(order.sequence > 0, "Order does not exist");
+        require(order.state == State.Accepted, "Order in incorrect state");
+        require(order.shipper == msg.sender);
+        orders[_orderId].state = State.Delivered;
+        emit Delivered(order.buyer, seller, order.shipper, order.shipment);
     }
 
-    function cancel() external payable onlyBuyer inState(State.Paid) {
-        bytes memory array = bytes(order);
-        bytes memory encoded = abi.encodePacked(array);
-        emit Canceled(buyer, seller, encoded);
-        state = State.Canceled;
+    function cancel(string memory _orderId) external payable {
+        Order storage order = orders[_orderId];
+        require(order.sequence > 0, "Order does not exist");
+        require(order.state == State.Submitted, "Order in incorrect state");
+        require(order.buyer == msg.sender);
+        orders[_orderId].state = State.Canceled;
+        emit Canceled(order.buyer, seller, order.shipper, _orderId);
     }
 
-    function abort() external payable onlySeller inState(State.Paid) {
-        bytes memory array = bytes(order);
-        bytes memory encoded = abi.encodePacked(array);
-        emit Aborted(buyer, seller, encoded);
-        state = State.Aborted;
+    function abort(string memory _orderId) external payable {
+        Order storage order = orders[_orderId];
+        require(order.sequence > 0, "Order does not exist");
+        require(order.state == State.Submitted, "Order in incorrect state");
+        require(seller == msg.sender);
+        orders[_orderId].state = State.Aborted;
+        emit Aborted(order.buyer, seller, order.shipper, _orderId);
     }
 
-    function withdrawalAllowed(address payee) public view returns (bool) {
+    function withdrawalAllowed(
+        string memory _orderId,
+        address payee
+    ) public view returns (bool) {
+        Order storage order = orders[_orderId];
         if (payee == seller) {
             return
-                state == State.Delivered &&
-                block.number >= (deliveredBlockNumber + WAIT_BLOCKS);
-        } else if (payee == shipper) {
+                order.state == State.Delivered &&
+                block.number >= (order.deliveredBlock + WAIT_BLOCKS);
+        } else if (payee == order.shipper) {
             return
-                state == State.Delivered &&
-                block.number >= (deliveredBlockNumber + WAIT_BLOCKS);
-        } else if (payee == buyer) {
-            return state == State.Canceled || state == State.Aborted;
+                order.state == State.Delivered &&
+                block.number >= (order.deliveredBlock + WAIT_BLOCKS);
+        } else if (payee == order.buyer) {
+            return
+                order.state == State.Canceled || order.state == State.Aborted;
         }
         return false;
     }
 
-    function withdraw(address payable payee) external {
-        require(withdrawalAllowed(payee), "payee is not allowed to withdraw");
-        uint256 payment = _deposits[payee];
-        _deposits[payee] = 0;
+    function withdraw(string memory _orderId, address payable payee) external {
+        Order storage order = orders[_orderId];
+        require(order.sequence > 0, "Order does not exist");
+        require(
+            withdrawalAllowed(_orderId, payee),
+            "payee is not allowed to withdraw"
+        );
+        uint256 payment = order.deposits[payee];
+        orders[_orderId].deposits[payee] = 0;
         require(
             address(this).balance >= payment,
             "Address: insufficient balance"
@@ -216,25 +239,5 @@ contract OrderProcessor {
             success,
             "Address: unable to send value, recipient may have reverted"
         );
-    }
-
-    function getCreatedBlockNumber() public view returns (uint256) {
-        return createdBlockNumber;
-    }
-
-    function getShippedBlockNumber() public view returns (uint256) {
-        return shippedBlockNumber;
-    }
-
-    function getDeliveredBlockNumber() public view returns (uint256) {
-        return deliveredBlockNumber;
-    }
-
-    function getOrder() public view returns (bytes memory) {
-        return order;
-    }
-
-    function getState() public view returns (State) {
-        return state;
     }
 }
