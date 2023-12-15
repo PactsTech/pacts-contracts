1e4ee837798b1ebec5418f28378a63f9b16cac25
Jake Breindel <j.breindel2@outlook.com>
2023-12-14 21:58:35 -0500

    chore: delete scripts

--- scripts/mint.js
+++
@@ -1,8 +0,0 @@
-import { Contract, Wallet } from 'ethers';
-import { proxyAddress, abi } from './usdc.js';
-
-const contract = new Contract(proxyAddress, abi);
-
-const resp = await contract.mint('0x5e91fef49a2811dbd0cc3ed2ed3a618e9efb58d0', 1000000);
-
-console.log({ resp });
--- scripts/mnemonic.js
+++
@@ -1,6 +0,0 @@
-import { Mnemonic, randomBytes } from 'ethers';
-
-const entropy = randomBytes(32);
-const mnemonic = Mnemonic.fromEntropy(entropy);
-
-console.log(mnemonic.phrase);
